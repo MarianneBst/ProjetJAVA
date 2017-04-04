e0084951d9f3a2eac014934f41fadb2a8ea358db
MarianneBst <marianne.bost@gmail.com>
2017-04-04 21:41:07 +0200

    Class test

---
+++ test/model/VirtualDepartmentTest.java
@@ -0,0 +1,27 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Marianne
+ * on 04/04/2017.
+ */
+public class VirtualDepartmentTest {
+    @Test
+    public void getNbEmployees() throws Exception {
+
+    }
+
+    @Test
+    public void getEmployeeByID() throws Exception {
+
+    }
+
+    @Test
+    public void addEmployee() throws Exception {
+
+    }
+
+}
