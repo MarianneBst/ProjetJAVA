672cfb496503a3f1037afd46d6c42ece372c873a
MarianneBst <marianne.bost@gmail.com>
2017-04-04 20:26:57 +0200

    Classes Person and Boss

--- src/model/Boss.java
+++ src/model/Boss.java
@@ -4,5 +4,10 @@ package model;
  * Created by Marianne
  * on 04/04/2017.
  */
-public class Boss {
+public class Boss extends Person{
+
+
+    public Boss(String name, String firstName) {
+        super(name, firstName);
+    }
 }
