be7902f201b2deeeeae957a4ac93c82ba023977c
MarianneBst <marianne.bost@gmail.com>
2017-04-04 21:01:17 +0200

    Class VirtualDepartment

--- src/model/VirtualDepartment.java
+++ src/model/VirtualDepartment.java
@@ -1,8 +1,42 @@
 package model;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 /**
  * Created by Marianne
  * on 04/04/2017.
  */
-public class VirtualDepartment {
+public abstract class VirtualDepartment {
+    private String name;
+    private HashMap<UUID, Employee> employeesList;
+
+    public VirtualDepartment(String name) {
+        this.name = name;
+        this.employeesList = new HashMap<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNbEmployees(){
+        return employeesList.size();
+    }
+
+    public Employee getEmployeeByID(UUID id){
+        // TODO: 04/04/2017 Exception if wrong id (null)
+        return employeesList.get(id);
+    }
+
+    public void addEmployee(Employee employee){
+        employeesList.put(employee.getId(), employee);
+    }
+
+
+
 }
